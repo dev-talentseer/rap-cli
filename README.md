927aa0b2f4b533f965c215ab988ea015512db57d
yong.talentseer <yong.su@talentseer.com>
2019-06-07 14:44:00 -0700

    :tada: (init) First commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Rap CLI
+
+[![Build Status](https://travis-ci.com/dev-talentseer/dawan.svg?token=CxF1Zpd3y8pJALXJ5RiT&branch=master)](https://travis-ci.com/dev-talentseer/dawan)
+
+CLI tool to create serverless project base on dawan-template.
+
+Integration: Travis CI, Commitizen, Serverless
+
+## Prerequisite
+
+   [Travis CI CLI Documentation](https://github.com/travis-ci/travis.rb)
+
+```bash
+brew install ruby
+gem install travis -v 1.8.10 --no-rdoc --no-ri
+travis login --pro
+```
+
+   [Commitizen documentation](https://github.com/commitizen/cz-cli)
+
+```bash
+npm install -g commitizen
+```
+
+## Installation
+
+```bash
+npm install -g @rap/rap-cli&&npm link
+```
+
+## Usage
+
+```bash
+rap init example
+```
+
+### Domain
+
+If enter `example.com` as domain and skipped basePath, the service will be deployed to:
+
+| Stage | Link |
+| --- | --- |
+| `dev` | <https://dev-api.example.com/api/hello-world> |
+| `prod` | <https://api.example.com/api/hello-world> |
+
+## Configuration
+
+### `.env` file
+
+Will be used for serverless offline environment and Travis CI environment.
+
+```bash
+DB_CONNECTION_STRING='<YOUR DB CONN STRING HERE>'
+AWS_ACCESS_KEY_ID='<YOUR AWS ACCESS KEY ID HERE>'
+AWS_SECRET_ACCESS_KEY='<YOUR AWS SECRET ACCESS KEY HERE>'
+```
+
+## Access Environment Variables
+
+Access database connection strings in your code with `${process.env.DB_CONNECTION_STRING}`
+
+Access stage (dev or prod) with `${process.env.STAGE}`
---
+++ index.js
@@ -0,0 +1,132 @@
+#!/usr/bin/env node
+const program = require('commander');
+const download = require('download-git-repo');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const handlebars = require('handlebars');
+const chalk = require('chalk');
+const ora = require('ora');
+const symbols = require('log-symbols');
+
+program.version('1.0.0', '-v, --version')
+    .command('init <name>')
+    .description(chalk.green('Initialize a new project with dawan-template'))
+    .action((name) => {
+        if (fs.existsSync(name)) {
+            // console.log(warning(`Target directory ${name} already exist.`));
+            console.log(chalk.bold.red('target directory exist'));
+            console.log(symbols.error, chalk.bold.red('project creating failed!'));
+            return 1;
+        }
+        inquirer.prompt([
+            {
+                name: 'description',
+                message: 'enter a description (optional)'
+            },
+            {
+                name: 'domain',
+                message: 'enter the domain you want to use [default: example.com] (optional)'
+            },
+            {
+                name: 'basePath',
+                message: 'enter the basePath for this service [default: api] (optional)'
+            },
+            {
+                name: 'runtime',
+                message: 'enter the runtime for this service [default: nodejs10.x] (optional)'
+            }
+        ]).then((answers) => {
+            const spinner = ora('downloading template...');
+            spinner.start();
+            download('github:dev-talentseer/dawan-template', name, { clone: true }, (err) => {
+                if (err) {
+                    spinner.fail();
+                    console.log(chalk.bold.red('error downloading template file'));
+                    console.log(symbols.error, chalk.bold.red('project creating failed!'));
+                    return 1;
+                }
+                spinner.succeed();
+                const meta = {
+                    name,
+                    description: answers.description || ''
+                };
+
+                const configSpinner = ora('creating .env file...');
+                configSpinner.start();
+                try {
+                    const domain = answers.domain || 'example.com';
+                    const basePath = answers.basePath || 'api';
+                    const runtime = answers.runtime || 'nodejs10.x';
+
+                    const variables = `DB_CONNECTION_STRING=''
+AWS_ACCESS_KEY_ID=''
+AWS_SECRET_ACCESS_KEY=''
+DOMAIN='${domain}'
+BASE_PATH='${basePath}'
+RUNTIME='${runtime}'`;
+                    fs.writeFileSync(`${name}/.env`, variables)
+                } catch (error) {
+                    configSpinner.fail();
+                    console.log(chalk.bold.red('error creating .env file'));
+                    console.log(symbols.error, chalk.bold.red('project creating failed!'));
+                    return 1;
+                }
+                configSpinner.succeed();
+
+                const pkgSpinner = ora('creating package.json file from template...');
+                pkgSpinner.start();
+                try {
+                    const fileName = `${name}/package.json`;
+                    const content = fs.readFileSync(fileName).toString();
+                    const result = handlebars.compile(content)(meta);
+                    fs.writeFileSync(fileName, result);
+                } catch (error) {
+                    pkgSpinner.fail();
+                    console.log(chalk.bold.red('error creating package.json file'));
+                    console.log(symbols.error, chalk.bold.red('project creating failed!'));
+                    return 1;
+                }
+                pkgSpinner.succeed();
+
+
+                const hookSpinner = ora('creating pre-push git hook...');
+                hookSpinner.start();
+                try {
+                    const dir = `${name}/.git/hooks`;
+                    fs.mkdirSync(dir, { recursive: true });
+                    const hookFileName = `${dir}/pre-push`;
+                    const scriptContent = `#!/bin/bash
+# read config
+source ../../.env
+
+if [ -z \${DB_CONNECTION_STRING} ]; then echo "DB_CONNECTION_STRING is unset" && exit 1; else travis env set DB_CONNECTION_STRING \${DB_CONNECTION_STRING}; fi
+if [ -z \${AWS_ACCESS_KEY_ID} ]; then echo "AWS_ACCESS_KEY_ID is unset" && exit 1; else travis env set AWS_ACCESS_KEY_ID \${AWS_ACCESS_KEY_ID}; fi
+if [ -z \${AWS_SECRET_ACCESS_KEY} ]; then echo "AWS_SECRET_ACCESS_KEY is unset" && exit 1; else travis env set AWS_SECRET_ACCESS_KEY \${AWS_SECRET_ACCESS_KEY}; fi
+if [ -z \${DOMAIN} ]; then echo "DOMAIN is unset" && exit 1; else travis env set DOMAIN \${DOMAIN}; fi
+if [ -z \${BASE_PATH} ]; then echo "BASE_PATH is unset" && exit 1; else travis env set BASE_PATH \${BASE_PATH}; fi
+if [ -z \${RUNTIME} ]; then echo "RUNTIME is unset" && exit 1; else travis env set RUNTIME \${RUNTIME}; fi
+`;
+                    fs.writeFileSync(hookFileName, scriptContent);
+                } catch (error) {
+                    hookSpinner.fail();
+                    console.log(error);
+                    console.log(chalk.bold.red('error creating pre-push git hook'));
+                    console.log(symbols.error, chalk.bold.red('project creating failed!'));
+                    return 1;
+                }
+                hookSpinner.succeed();
+                console.log(symbols.success, chalk.green('project created successfully! skr skr'));
+
+            })
+        })
+    });
+program.parse(process.argv);
+
+// Check the program.args obj
+var NO_COMMAND_SPECIFIED = program.args.length === 0;
+
+// Handle it however you like
+if (NO_COMMAND_SPECIFIED) {
+    // e.g. display usage
+    program.help();
+}
